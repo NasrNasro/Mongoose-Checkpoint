9e5a4d16508ad62b7f427feba91ff85dc1cb31f0
Nassreddine Hnana <nasrihnana@gmail.com>
2022-04-19 14:35:10 +0200

    adding checkpoint REST API

--- models/contact.js
+++ models/contact.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose')
 const ContactSchema= new mongoose.Schema({
     name:{type:String, required:true},
     age:Number,
-    email:{type:String, unique:true, required:true}
+    email:{type:String, unique:true, required:true},
+    gender:{type:String, required:true}
 })
 
 module.exports=mongoose.model("Contact", ContactSchema)
